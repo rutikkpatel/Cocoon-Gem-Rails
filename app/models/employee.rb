6201d9011c1fd2380521a94a009ef8e1a403c9c8
rutikpatel20 <rutik.p@simformsolutions.com>
2023-02-17 01:33:54 +0530

    edit model and controller file for dynamic fields

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+  has_many :addresses
+  accepts_nested_attributes_for :addresses,
+                                allow_destroy: true,
+                                reject_if: proc { |att| att['house_number'].blank? || ['society_name'].blank? || ['city'].blank? || ['area'].blank? }
 end
